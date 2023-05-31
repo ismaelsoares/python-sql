5750412c30c05765e2f13b411ea882c879ba00c8
ismaelsoares <ismaelsoaresdoria@gmail.com>
2023-05-31 15:05:49 -0300

    Versão inicial do projeto

---
+++ app.py
@@ -0,0 +1,28 @@
+import pyodbc
+# Drive - Drive
+# Server - Server
+# Database - Nome do Banco de Dados
+dados_conexao = (
+    "DRIVER={SQLite3 ODBC Driver};SERVER=localhost;DATABASE=./database/projeto_compras.db;Trusted_connection=yes")
+
+
+# Criando a conexão
+conexao = pyodbc.connect(dados_conexao)
+
+print("Conectado com sucesso!")
+
+# cursor - Ferramenta para executar os comandos em SQL
+cursor = conexao.cursor()
+
+# Selecionando a tabela do banco de dados
+cursor.execute("SELECT * FROM usuarios")
+# Passar os dados para a variável
+valores = cursor.fetchall()
+
+produtos = cursor.execute("SELECT * FROM produtos").fetchall()
+
+for i in valores:
+    for k in i:
+        print(k)
+
+print(produtos)
